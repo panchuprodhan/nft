6b27f201087ff874753d688bf1eb6f6f9f474586
panchuprodhan <41304874+panchuprodhan@users.noreply.github.com>
2022-10-28 16:56:38 +0530

    Update nft.sol

--- nft.sol
+++ nft.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract NFTToken is ERC721URIStorage {
